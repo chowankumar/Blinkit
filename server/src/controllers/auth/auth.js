cc9f520346587821de60210f4002cca717f472b8
chowan kumar <chowanmalhani5@gmail.com>
2024-11-09 15:26:14 +0500

    create the controller for the loginCustomer

--- server/src/controllers/auth/auth.js
+++ server/src/controllers/auth/auth.js
@@ -1,7 +1,7 @@
 import { Customer,DeliveryPartner } from "../../models";
 import jwt from "jsonwebtoken";
 
-const generateToken = (user)=>{
+const generateTokens = (user)=>{
 
 
     const accessToken = jwt.sign(
@@ -15,7 +15,37 @@ const generateToken = (user)=>{
         {userId: user._id, role : user.role},
         process.env.REFRESH_TOKEN_SECRET,
         {expiresIn : "1d"}
-
+ 
     );
     return {refreshToken,accessToken}
 };
+
+
+export const loginCustomer = async(req,reply)=>{
+    try {
+        const  {phone} = req.body;
+        let customer = await Customer.findOne({phone});
+
+        if(!customer){
+            customer = new Customer({
+                phone,
+                role:"Customer",
+                isActivated: true,
+            })
+            await customer.save();
+        }
+
+        const {accessToken,refreshToken}= generateTokens(customer);
+        
+        return reply.send({
+          message : customer ? "Login Successfully " : "Customer created and logged in",
+          accessToken,
+          refreshToken,
+          customer,
+        })
+    } catch (error) {
+
+        return reply.status(500).send({message : "an error accoured in login",error})
+        
+    }
+}
